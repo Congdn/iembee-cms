21dc112e252180e47a88d5c3794de7a3212278f2
Congdn <43802528+Congdn@users.noreply.github.com>
2020-03-09 11:54:54 +0700

    a

---
+++ iembee/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace iembee.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly string UserName = "admin";
+        private readonly string Password = "@admin";
+        // GET: Account
+        public ActionResult Index()
+        {
+            if (!string.IsNullOrEmpty(HttpContext.User.Identity.Name))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, bool memberme = false)
+        {
+            if(username == UserName && password == Password)
+            {
+                FormsAuthentication.SetAuthCookie(username, memberme);
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Index");
+        }
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return View("Index");
+        }
+    }
+}
--- iembee/Controllers/HomeController.cs
+++ iembee/Controllers/HomeController.cs
@@ -1,30 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace iembee.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExportFile(Data model)
         {
-            ViewBag.Message = "Your application description page.";
+            var filePath = "";
+            //Save upload file
+            if (model.fileInput.ContentLength > 0)
+            {
+                var filename = Path.GetFileName(model.fileInput.FileName);
+                filePath = Path.Combine(Server.MapPath("~/App_Data/uploads"), filename);
+                model.fileInput.SaveAs(filePath);
+            }
 
-            return View();
+            //Export data
+
+
+            //var body = request.Content.ReadAsByteArrayAsync();
+            return Content("test");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+    }
+    public class Data
+    {
+        [Required(ErrorMessage ="File does not exist")]
+        public HttpPostedFileBase fileInput { get; set; }
+        [Required]
+        public int hangtoithieu { get; set; }
+        [Required]
+        public int hangtoida { get; set; }
+        public string tenkh { get; set; }
+        public string diachi { get; set; }
+        public string dienthoai { get; set; }
+        public int? tongnhap3 { get; set; }
+        public int? tongnhap2 { get; set; }
+        public int? tongnhap1 { get; set; }
+        public int? tongxuat3 { get; set; }
+        public int? tongxuat2 { get; set; }
+        public int? tongxuat1 { get; set; }
     }
 }
